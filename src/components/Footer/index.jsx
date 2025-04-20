461c8e99ed2163c2f3b68ee5d8b6609e0d536c03
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-04-20 19:26:28 +0530

    Updated Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,40 +2,62 @@ import React from "react";
 import styles from "./Footer.module.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
-import { Box, IconButton, Link, Typography } from "@mui/material";
+import { Box, Divider, IconButton, Link, Typography } from "@mui/material";
 
 function Footer() {
   return (
     <Box className={styles.footer}>
-      <IconButton
-        sx={{ color: "var(--secondary-color)" }}
-        component="a"
-        href="mailto:dhanrajaayush123@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <EmailIcon fontSize="small" />
-      </IconButton>
-      <Typography variant="subtitle1">
-        <Link
-          href="https://3d-portfolio-demo-delta.vercel.app/"
+      <Box>
+        <Typography variant="subtitle1">
+          <Link
+            href="https://3d-portfolio-demo-delta.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            color="inherit"
+            underline="hover"
+          >
+            © {new Date().getFullYear()} @ ME, Myself and I
+          </Link>
+        </Typography>
+      </Box>
+      <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
+        <IconButton
+          sx={{ color: "var(--secondary-color)" }}
+          component="a"
+          href="mailto:dhanrajaayush123@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
-          color="inherit"
-          underline="hover"
         >
-          Ayush Dhanraj
-        </Link>
-      </Typography>
-      <IconButton
-        sx={{ color: "var(--secondary-color)" }}
-        component="a"
-        href="https://github.com/Ayush-2001-Dhanraj"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <GitHubIcon fontSize="small" />
-      </IconButton>
+          <EmailIcon fontSize="small" />
+        </IconButton>
+        <IconButton
+          sx={{ color: "var(--secondary-color)" }}
+          component="a"
+          href="https://github.com/Ayush-2001-Dhanraj"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon fontSize="small" />
+        </IconButton>
+      </Box>
+      <Divider
+        orientation="vertical"
+        flexItem
+        sx={{ borderColor: "var(--primary-color)" }}
+      />
+      <Box>
+        <Typography variant="subtitle1">
+          <Link
+            href="https://3d-portfolio-demo-delta.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            color="inherit"
+            underline="hover"
+          >
+            Ayush Dhanraj
+          </Link>
+        </Typography>
+      </Box>
     </Box>
   );
 }
